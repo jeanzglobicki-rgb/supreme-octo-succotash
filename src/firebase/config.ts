65c38441083f6df5b260470da46155cc3448540d
Jean-Pierre Zglobicki <jean.zglobicki@gmail.com>
2025-11-07 13:07:43 +0000

    FirebaseError: Firebase: Error (auth/invalid-api-key).     at createErro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,12 +2,11 @@
 import { FirebaseOptions } from 'firebase/app';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  projectId: "studio-5091361466-c0227",
+  appId: "1:984620828653:web:682f36819018c0e54c49e4",
+  apiKey: "AIzaSyBLIgycgfkypuEU8DnLBP-UH8SZ6zIR4D8",
+  authDomain: "studio-5091361466-c0227.firebaseapp.com",
+  messagingSenderId: "984620828653"
 };
 
 export default firebaseConfig;
