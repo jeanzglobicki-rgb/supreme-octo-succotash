0c5c70a373c5c4cbf088a88169c0c64926f01d6c
Jean-Pierre Zglobicki <jean.zglobicki@gmail.com>
2025-11-07 13:47:59 +0000

    ok lets leave the share button im ready to upload this app to the play s

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { FirebaseErrorListener } from '@/components/FirebaseErrorListener';
 export const metadata: Metadata = {
   title: 'Daily Script',
   description: 'Daily Bible Verses & Random Verse App',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -22,6 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Literata:opsz@7..72&display=swap" rel="stylesheet" />
+        <meta name="theme-color" content="#7c7bad" />
       </head>
       <body className="font-body antialiased min-h-screen flex flex-col">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
